d654d9496fe2feda47a25b724e9a0a33c2946e36
David Mandelberg <david@mandelberg.org>
2018-12-25 12:35:36 -0500

    Add keyboard shortcuts for play/pause, prev, and next.

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,29 @@
+;; Copyright 2018 Google LLC
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     https://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+
+;; Binds a key to a key sequence, using xdotool.
+(define (xbindkey-keys key xdotool-keys)
+  (let ((xdotool-key->xdotool-key-arg
+          (lambda (key) (string-join (map symbol->string key) "+"))))
+    (xbindkey
+      key
+      (string-join
+        `("xdotool key --clearmodifiers"
+          ,@(map xdotool-key->xdotool-key-arg xdotool-keys))))))
+
+
+(xbindkey-keys '(release alt comma) '((XF86AudioPrev)))
+(xbindkey-keys '(release alt period) '((XF86AudioNext)))
+(xbindkey-keys '(release alt slash) '((XF86AudioPlay)))
