cb1f43844344ffd98b326fbb98ff2b57bf11c11d
David Mandelberg <david@mandelberg.org>
2019-01-19 00:16:52 -0500

    Improve prompt status.
    1. Only show command return values immediately after a foreground-only
       command.
    2. Show $! after anything is put in the background.

--- .config/shell/interactive.d/50-prompt-80-status.bash
+++ .config/shell/interactive.d/50-prompt-80-status.bash
@@ -13,19 +13,8 @@
 # limitations under the License.
 
 
-# Appends the given return value, with color depending on the value.
-__prompt_part_status_append_ret() {
-  if [[ "$1" = 0 ]]; then
-    prompt_append "$1" "$FgGreen"
-  else
-    prompt_append "$1" "$FgBrRed"
-  fi
-}
-
-# Show counts of running and stopped jobs, and the return value of the previous
-# command.
+# Show counts of running and stopped jobs, and info about the previous command.
 __prompt_part_status() {
-  local command_ret=$?
   local running_jobs=$(shrcutil_count_args $(jobs -rp))
   local stopped_jobs=$(shrcutil_count_args $(jobs -sp))
 
@@ -33,21 +22,27 @@ __prompt_part_status() {
   [[ "$running_jobs" != 0 ]] && show_jobs=yes
   [[ "$stopped_jobs" != 0 ]] && show_jobs=yes
 
-  local show_ret=
-  local component_ret
-  for component_ret in "$command_ret" "${BP_PIPESTATUS[@]}"; do
-    if [[ "$component_ret" != 0 ]]; then
-      show_ret=yes
-      break
-    fi
-  done
+  local show_ret_or_bg=
+  if [[ -z "$BPE_FIRST_PROMPT_AFTER_COMMAND" ]]; then
+    :
+  elif [[ -n "$BPE_LAST_COMMAND_DID_BG" ]]; then
+    show_ret_or_bg=yes
+  else
+    local component_ret
+    for component_ret in "${BP_PIPESTATUS[@]}"; do
+      if [[ "$component_ret" != 0 ]]; then
+        show_ret_or_bg=yes
+        break
+      fi
+    done
+  fi
 
-  if [[ -z "$show_ret" ]] && [[ -z "$show_jobs" ]]; then
+  if [[ -z "$show_ret_or_bg" ]] && [[ -z "$show_jobs" ]]; then
     return
   fi
 
   local show_divider=
-  [[ -n "$show_ret" ]] && [[ -n "$show_jobs" ]] && show_divider=yes
+  [[ -n "$show_ret_or_bg" ]] && [[ -n "$show_jobs" ]] && show_divider=yes
 
   prompt_append_raw ' ['
   if [[ -n "$show_jobs" ]]; then
@@ -56,26 +51,25 @@ __prompt_part_status() {
     prompt_append "$stopped_jobs" "$FgBrYellow"
   fi
   [[ -n "$show_divider" ]] && prompt_append_raw '|'
-  if [[ -n "$show_ret" ]]; then
-    # TODO: Figure out how to show $PIPESTATUS/$? only if they came from the
-    # command immediately before this prompt. E.g., $PIPESTATUS is not affected
-    # by running `foo &`, and neither variable is affected by hitting Enter
-    # without any command.
-    local is_first_component=yes
-    local last_component_ret=
-    local component_ret
-    for component_ret in "${BP_PIPESTATUS[@]}"; do
-      if [[ -n "$is_first_component" ]]; then
-        is_first_component=
-      else
-        prompt_append_raw '-'
-      fi
-      __prompt_part_status_append_ret "$component_ret"
-      last_component_ret="$component_ret"
-    done
-    if [[ "$last_component_ret" != "$command_ret" ]]; then
-      prompt_append_raw ':'
-      __prompt_part_status_append_ret "$command_ret"
+  if [[ -n "$show_ret_or_bg" ]]; then
+    if [[ -n "$BPE_LAST_COMMAND_DID_BG" ]]; then
+      prompt_append "$!" "$FgMagenta"
+      prompt_append_raw '&'
+    else
+      local is_first_component=yes
+      local component_ret
+      for component_ret in "${BP_PIPESTATUS[@]}"; do
+        if [[ -n "$is_first_component" ]]; then
+          is_first_component=
+        else
+          prompt_append_raw '-'
+        fi
+        if [[ "$component_ret" = 0 ]]; then
+          prompt_append "$component_ret" "$FgGreen"
+        else
+          prompt_append "$component_ret" "$FgBrRed"
+        fi
+      done
     fi
   fi
   prompt_append_raw ']'
