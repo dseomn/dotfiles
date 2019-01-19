94196a5ddb2d360657fdc4a2686d52cbe00f4371
David Mandelberg <david@mandelberg.org>
2019-01-19 13:38:57 -0500

    Keep a persistent log of all shell commands.

--- .config/shell/interactive.d/50-history.bash
+++ .config/shell/interactive.d/50-history.bash
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 
+# Configure built-in history.
 __set_at_least() {
   # Ensure that the environment variable named in $1 is greater than or equal
   # to $2.
@@ -25,3 +26,47 @@ __set_at_least HISTFILESIZE $((100 * 1000))
 shopt -s histappend
 
 unset -f __set_at_least
+
+
+# Configure custom history logs.
+__history_prompt_index=-1
+__history_command=
+
+__history_fill_common_args() {
+  args+=(
+      "pid=${$}"
+      "prompt_index=${__history_prompt_index}"
+      "shell=${BASH:-bash}"
+      "pwd=${PWD}"
+      "command=${__history_command}"
+  )
+}
+
+__history_preecmd() {
+  if [[ -n "$__history_command" ]]; then
+    local args
+    __history_fill_common_args
+    args+=("stage=command_end")
+    if [[ -n "$BPE_LAST_COMMAND_DID_BG" ]]; then
+      args+=("command_pid=${!}")
+    else
+      args+=("command_retvals=${BP_PIPESTATUS[*]}")
+    fi
+    shell-history append "${args[@]}"
+    __history_command=
+  fi
+  let ++__history_prompt_index
+}
+
+precmd_functions+=(__history_preecmd)
+
+__history_preexec() {
+  __history_command="$1"
+  local args
+  __history_fill_common_args
+  args+=("stage=command_begin")
+  shell-history append "${args[@]}"
+  return 0  # Don't prevent the command from running when history saving fails.
+}
+
+preexec_functions+=(__history_preexec)
