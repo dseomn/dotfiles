5d2189d2620eca6ac0286fc261ac5f204ba1dbc0
David Mandelberg <david@mandelberg.org>
2019-01-19 00:02:48 -0500

    Add some useful variables for precmd_functions.

--- .config/shell/interactive.d/20-bash-preexec.bash
+++ .config/shell/interactive.d/20-bash-preexec.bash
@@ -14,3 +14,56 @@
 
 
 shrcutil_source ~/third_party/bash-preexec/bash-preexec.sh || return
+
+
+# The rest of this file provides additional variables to preexec and precmd
+# functions. These public variables all start with BPE_, short for Bash Preexec
+# Extra.
+
+
+# BPE_FIRST_PROMPT_AFTER_COMMAND (precmd): This will be non-empty in the first
+# precmd run after a command, and empty in all other precmd runs. E.g., if
+# interactive.d contains this code:
+#
+#   f() { [[ -n "$BPE_FIRST_PROMPT_AFTER_COMMAND" ]] && echo set; }
+#   precmd_functions+=(f)
+#
+# Then an interactive session could look like:
+#
+#   $
+#   $ ^C
+#   $ :
+#   set
+#   $
+__bpe_set_first_prompt_after_command() {
+  BPE_FIRST_PROMPT_AFTER_COMMAND=true
+}
+
+__bpe_late_preexec_functions+=(__bpe_set_first_prompt_after_command)
+
+__bpe_clear_first_prompt_after_command() {
+  BPE_FIRST_PROMPT_AFTER_COMMAND=
+}
+
+__bpe_late_precmd_functions+=(__bpe_clear_first_prompt_after_command)
+
+__bpe_clear_first_prompt_after_command
+
+
+# BPE_LAST_COMMAND_DID_BG (precmd): This will be non-empty iff the most recent
+# command run put something into the background.
+BPE_LAST_COMMAND_DID_BG=
+
+__bpe_last_bg_pid="$!"
+
+__bpe_set_last_command_did_bg() {
+  [[ -n "$BPE_FIRST_PROMPT_AFTER_COMMAND" ]] || return 0
+  if [[ "$!" = "$__bpe_last_bg_pid" ]]; then
+    BPE_LAST_COMMAND_DID_BG=
+  else
+    BPE_LAST_COMMAND_DID_BG=true
+  fi
+  __bpe_last_bg_pid="$!"
+}
+
+precmd_functions+=(__bpe_set_last_command_did_bg)
