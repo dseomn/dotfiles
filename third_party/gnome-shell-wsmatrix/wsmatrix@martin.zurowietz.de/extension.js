d85b2213bfcce202e8eca523ec5a8e65f9996b5a
David Mandelberg <david@mandelberg.org>
2020-03-16 14:08:47 +0100

    Project import generated by Copybara.
    GitOrigin-RevId: 0a6bd59823fe8fde8df12aa46da6497f76443158

---
+++ third_party/gnome-shell-wsmatrix/wsmatrix@martin.zurowietz.de/extension.js
@@ -0,0 +1,31 @@
+const WsMatrix = imports.misc.extensionUtils.getCurrentExtension();
+const Lang = imports.lang;
+const Meta = imports.gi.Meta;
+const Settings = WsMatrix.imports.Settings.Settings;
+const WmOverride = WsMatrix.imports.WmOverride.WmOverride;
+const OverviewOverride = WsMatrix.imports.OverviewOverride.OverviewOverride;
+
+class WsMatrixExtension {
+   constructor() {
+      let settings = new Settings(WsMatrix.metadata['settings-schema']);
+      let keybindings = new Settings(WsMatrix.metadata['keybindings-schema']);
+      this.overrideWorkspace = new WmOverride(settings, keybindings);
+      this.overrideOverview = new OverviewOverride(settings, keybindings);
+   }
+
+   destroy() {
+      this.overrideWorkspace.destroy();
+      this.overrideOverview.destroy();
+   }
+}
+
+let wsMatrix;
+
+function enable() {
+   wsMatrix = new WsMatrixExtension();
+}
+
+function disable() {
+   wsMatrix.destroy();
+   wsMatrix = null;
+}
