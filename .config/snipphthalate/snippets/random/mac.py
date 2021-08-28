e0bbe754cabcdd4481bd4c46a8597e93c1cbfccd
David Mandelberg <david@mandelberg.org>
2021-08-28 17:29:04 -0400

    Use different regular tags instead of variants

--- .config/snipphthalate/snippets/random/mac.py
+++ .config/snipphthalate/snippets/random/mac.py
@@ -12,35 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import random
 import secrets
 
 from snipphthalate import plugin
 
 
-def _random_mac():
-  # https://en.wikipedia.org/wiki/MAC_address
-  octets = list(secrets.token_bytes(nbytes=6))
-  octets[0] |= 0x02  # Locally-administered.
-  octets[0] &= ~0x01  # Unicast.
-  return ':'.join(f'{octet:02x}' for octet in octets)
-
-
-_VARIANTS = {
-    'hex4': lambda: secrets.token_hex(nbytes=4),
-    'mac': _random_mac,
-    'port': lambda: str(random.randint(1024, 49151)),
-}
-
-
-class RandomPlugin(plugin.Jinja2SnippetPlugin):
-
-  def __init__(self):
-    super().__init__()
-    self.variants[:] = _VARIANTS
-
-  def is_applicable(self, snipphthalate_context, tag, variant):
-    return variant is not None
+class RandomMacPlugin(plugin.Jinja2SnippetPlugin):
 
   def update_context(self, jinja2_context, snipphthalate_context, variant):
-    jinja2_context['value'] = _VARIANTS[variant]()
+    # https://en.wikipedia.org/wiki/MAC_address
+    octets = list(secrets.token_bytes(nbytes=6))
+    octets[0] |= 0x02  # Locally-administered.
+    octets[0] &= ~0x01  # Unicast.
+    jinja2_context['random_mac'] = ':'.join(f'{octet:02x}' for octet in octets)
