82411c20623007b25265c1e6317eca6917e429b2
David Mandelberg <david@mandelberg.org>
2025-11-26 19:42:26 -0500

    Update datetime connector key name
    https://bugzilla.mozilla.org/show_bug.cgi?id=1706318

--- .thunderbird/all/user.js
+++ .thunderbird/all/user.js
@@ -36,7 +36,7 @@ user_pref('intl.date_time.pattern_override.date_short', 'yyyy-MM-dd');
 user_pref('intl.date_time.pattern_override.date_medium', 'd MMM y');
 user_pref('intl.date_time.pattern_override.date_long', 'd MMMM y');
 user_pref('intl.date_time.pattern_override.date_full', 'EEEE d MMMM y');
-user_pref('intl.date_time.pattern_override.date_time_short', '{1} {0}');
+user_pref('intl.date_time.pattern_override.connector_short', '{1} {0}');
 user_pref('intl.locale.requested', '');
 
 // Calendar.
